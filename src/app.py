5bd88f7e188802253e6d82c080b276ef39ba47ac
Liam <liam@lclamedia.com>
2024-05-06 12:40:59 +1000

    feat: add postgres status updates

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import logging
 import os
 import threading
+import uuid
 
 from GoogleDocsOperations import GoogleDocsOperations
 from NotionOperator import NotionOperator
@@ -9,25 +10,22 @@ from VoiceflowOperations import VoiceflowOperations
 from ProposalScreeningOperations import ProposalScreeningOperations
 from PromptsOperations import PromptsOperations
 from GPTOperations import GPTOperations
+from PostgresOperations import PostgresOperations
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-@app.route("/create_report_document", methods=["POST"])
-def create_report_document():
-    inputs = request.json
-    # Create document and return the URL for output
-    notion_ops = NotionOperator()
-    notion_ops.create_test_page()
-    return {}, 200
+@app.route('/status/<task_id>', methods=['GET'])
+def check_status(task_id):
+    status_table = PostgresOperations(dbname='postgres',user='automations',password=os.getenv('POSTGRES_PASSWORD'),host='34.116.78.6')
+    return status_table.get_status(task_id)
 
-@app.route("/check_status")
-
-def run_analysis(url: str, page_id: str):
+def run_analysis(url: str, page_id: str, run_id: str):
     prompts_ops = PromptsOperations()
     gpt_ops = GPTOperations(prompts_ops=prompts_ops)
     notion_ops = NotionOperator()
+    
     
     proposal_ops = ProposalScreeningOperations(
         proposal_url=url,
@@ -40,22 +38,28 @@ def run_analysis(url: str, page_id: str):
     )
     
     proposal_ops.run()
+    
+    status_table = PostgresOperations(dbname='postgres',user='automations',password=os.getenv('POSTGRES_PASSWORD'),host='34.116.78.6')
+    status_table.complete_task(run_id)
 
 
 @app.route("/analyse_proposal", methods=["POST"])
 def analyse_proposal():
     inputs = request.json
     
+    # Create Run Entry
+    status_table = PostgresOperations(dbname='postgres',user='automations',password=os.getenv('POSTGRES_PASSWORD'), host='34.116.78.6')
+    id = status_table.initiate_run()
+    
     notion_ops = NotionOperator()
     page_id, page_url = notion_ops.create_blank_page(inputs.get("title"))
     
     # Start the background task
-    thread = threading.Thread(target=run_analysis, args=(inputs.get('url'), page_id))
+    thread = threading.Thread(target=run_analysis, args=(inputs.get('url'), page_id, id))
     thread.start()
     
-    return {'url': page_url}, 200
+    return {'url': page_url, 'status_id': id}, 200
     
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-    
